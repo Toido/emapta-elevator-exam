50fb5510cb7450086cc9901751c360afa05126f3
daryl-dev <caguiadaryl@gmail.com>
2024-02-09 14:14:11 +0800

    Setup test env and config

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  moduleFileExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { module: 'esnext' }],
+  },
+  testMatch: ['**/*.test.(js|jsx|ts|tsx)'],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  verbose: true,
+  moduleDirectories: ['node_modules', 'src', 'src/apis'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  rootDir: 'src',
+};
